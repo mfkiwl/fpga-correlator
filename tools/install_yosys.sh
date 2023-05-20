62775784fc95dfc1e0d5d9bedd8ec918fa48bd53
Tim Molteno <tim@4i.nz>
2023-05-21 10:48:09 +1200

    Use pipx as pip3 install is deprecated for system-wide tools

--- tools/install_yosys.sh
+++ tools/install_yosys.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-pipx install yowasp-yosys
+# Install the tools required to build the FPGA
+#
+pipx install yowasp-yosys 
 pipx install yowasp-nextpnr-gowin
-pipx upgrade yowasp-yosys
+pipx upgrade yowasp-yosys 
 pipx upgrade yowasp-nextpnr-gowin
